783c0af95bfdfa2ce50686bfa550f429090813f1
Megan Coyle <mcoyle@usgbc.org>
2017-03-21 09:51:47 -0400

    Add models

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+# third-party imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# local imports
+from config import app_config
+
+# db variable initialization
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    # temporary route
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+
+    return app
