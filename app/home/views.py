dbd85f8a77a4b9104c8397b8cd29acb71dba5852
Megan Coyle <mcoyle@usgbc.org>
2017-03-22 11:21:09 -0400

    Add ability to add, edit, and delete departments

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import current_user, login_required
 
 from . import home
 
@@ -18,3 +18,12 @@ def dashboard():
     """
 
     return render_template('home/dashboard.html', title="Dashboard")
+
+@home.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    # prevent non-admins from accessing the page
+    if not current_user.is_admin:
+        abort(403)
+
+    return render_template('home/admin_dashboard.html', title="Dashboard")
