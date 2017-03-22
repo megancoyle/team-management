6b6f1b3519b520d82ee2e7e00daed7bff0e06f71
Megan Coyle <mcoyle@usgbc.org>
2017-03-22 15:48:04 -0400

    Add unit testing

--- config.py
+++ config.py
@@ -3,15 +3,14 @@ class Config(object):
     Common configurations
     """
 
+    DEBUG = True
 
 class DevelopmentConfig(Config):
     """
     Development configurations
     """
 
-    DEBUG = True
     SQLALCHEMY_ECHO = True
-
 
 class ProductionConfig(Config):
     """
@@ -20,7 +19,15 @@ class ProductionConfig(Config):
 
     DEBUG = False
 
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+
 app_config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig
+    'production': ProductionConfig,
+    'testing': TestingConfig
 }
